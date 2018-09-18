c78d34aeeff990f7bc216682e65ce27ea710ea34
Panupong Chantaklang <nancomsp@gmail.com>
2018-09-18 11:50:20 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "-= Start =-"
-exec docker run --rm --name micro-beat --memory=128m -v /Users/nancom/Tools/Docker/Compose/boot-beat/run.sh:/opt/app/run.sh -v /Users/nancom/Tools/Docker/Compose/boot-beat/SMS-0.99.jar:/opt/app/run.jar -p 8888:8892 nancom/microjava-beat:v2.1
+exec docker run --rm --name micro-beat --memory=128m -v $filepath/run.sh:/opt/app/run.sh -v $filepath/some-jar.jar:/opt/app/run.jar -p 8888:8892 nancom/microjava-beat:v2.1
